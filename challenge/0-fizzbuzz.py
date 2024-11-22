f7e1c4181aea81f65202cb0e3d3ff581412524b0
KhairiTab <taboubikhairi68@gmail.com>
2024-11-22 14:38:16 +0100

    fizzbuzz

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""FizzBuzz
-
-Prints numbers from 1 to n, replacing multiples of 3 with "Fizz",
-multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".
+""" FizzBuzz
 """
 
 import sys
@@ -10,27 +7,27 @@ import sys
 
 def fizzbuzz(n):
     """
-    Prints the FizzBuzz sequence for numbers 1 to n.
+    FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    Args:
-        n: The upper limit of the sequence.
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
-
+    
     if n < 1:
         return
 
-    result = []
+    tmp_result = []
     for i in range(1, n + 1):
-        if i % 15 == 0:
-            result.append("FizzBuzz")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
         else:
-            result.append(str(i))
-
-    print(" ".join(result))
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
